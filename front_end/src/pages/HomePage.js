8f93097788776888ca2648c907f49ee08f809e19
marshalyordanos <marshalyordanosseifu@gmail.com>
2022-08-28 17:42:09 +0300

    add toggle and other functionality to the filter component

--- front_end/src/pages/HomePage.js
+++ front_end/src/pages/HomePage.js
@@ -1,10 +1,29 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-
+import Filter from "../components/Filter";
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton } from "@mui/material";
 const HomePage = () => {
+  const [filterToggle, setFilterToggle] = React.useState(false);
+  const filterHandler = () => {
+    setFilterToggle(!filterToggle);
+  };
   return (
     <div>
-      <NavBar />
+      {/**************************************************  navbar  ***********************************************  */}
+      <div>
+        <NavBar handleFilter={filterHandler} />
+      </div>
+
+      {/************************************************** filter *************************************************  */}
+
+      {filterToggle && (
+        <div className="">
+          <div>
+            <Filter />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
